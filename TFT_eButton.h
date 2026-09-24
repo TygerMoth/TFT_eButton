206b0caf289ac44d51847a2fcbd2dee4c77e3be0
Tyger Moth <146899581+TygerMoth@users.noreply.github.com>
2026-09-24 12:02:39 -0400

    Fix formatting and alignment in TFT_eButton.h

--- TFT_eButton.h
+++ TFT_eButton.h
@@ -77,8 +77,8 @@ class TFT_eButton : public TFT_eSPI {
   uint16_t _outlinecolor, _fillcolor, _textcolor, _outlinewidth, _bgcolor;
   char     _label[10]; // Button text is 9 chars maximum unless long_name used
   uint32_t _pressTime, _releaseTime;
-  bool  _inverted, currstate, laststate, enblstate, vsblstate; // Button states
-  void drawDisabledInternal();
+  bool     _inverted, currstate, laststate, enblstate, vsblstate; // Button states
+  void     drawDisabledInternal();
 };
 
 #endif
